94e2feee9f42e4053a0bc1b90381e3552526070f
Gr03a <diadonapp@gmail.com>
2026-09-10 20:47:03 +0300

    первые 3 задания
    во время пары были выполнены первые три пункта лабораторной работы. выполнено: Димов Н., Бардычев А.

---
+++ lab1.cpp
@@ -0,0 +1,44 @@
+﻿#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+#include <locale.h>
+
+
+int main(void)
+{
+    setlocale(LC_ALL, "rus");
+    int min, max;
+    // 3 задание
+    int n;
+    printf("Введите размер массива: ");
+    if (scanf("%d", &n) != 1  n <= 0) {
+        printf("\n Некорректный размер массива!\n");
+        return 1;
+    }
+
+    int* array = (int*)malloc(n * sizeof(int));
+    if (array == NULL) {
+        printf("Ошибка выделения памяти!\n");
+        return 1;
+    }
+
+    // 2 задание
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = -20 + rand() % 61;
+        printf("%d;", array[i]);
+    }
+    //1 задание
+    max = min = array[0];
+    for (int i = 0; i < n; i++) {
+        if (array[i] < min) { min = array[i]; }
+        if (array[i] > max) { max = array[i]; }
+    }
+    printf("\n max: %d \n min: %d \n max-min=%d \n", max, min, (max - min));
+    free(array);
+    return 0;
+}
