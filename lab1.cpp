1ebc889a64f0b8cf738304808c742101fbf3cbe1
Artyshi <60825393+Art-SHOCK@users.noreply.github.com>
2026-09-10 20:52:39 +0300

    Выполнил 5 задание
    Выполнил Бардычев А.

--- lab1.cpp
+++ lab1.cpp
@@ -5,6 +5,14 @@
 #include <math.h>
 #include <time.h>
 #include <locale.h>
+#include <string.h>
+
+struct student
+{
+	int Nomzach;
+	char famil[20];
+	char name[20], facult[20];
+};
 
 
 int main(void)
@@ -54,5 +62,51 @@ int main(void)
     }
     printf("%4d\n",sum);
   }
+    	// 5 задание
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	int i;
+	struct student
+	{
+		char famil[20];
+		char name[20], facult[20];
+		int Nomzach;
+	} stud[3];
+
+	for (i = 0; i<3; i++)
+	{
+		printf("Введите фамилию студента\n"); scanf("%20s", stud[i].famil);
+	}
+	for (i = 0; i<3; i++)
+	{
+		printf("Введите имя студента %s\n", stud[i].famil); scanf("%20s", stud[i].name);
+	}
+	for (i = 0; i<3; i++)
+	{
+		printf("Введите название факультета студента %s %s\n", stud[i].famil, stud[i].name); scanf("%20s", stud[i].facult);
+	}
+	for (i = 0; i<3; i++)
+	{
+		printf("Введите номер зачётной книжки студента %s %s\n", stud[i].famil, stud[i].name); scanf("%d", &stud[i].Nomzach);
+	}
+
+	for (i = 0; i<3; i++)
+	{
+		printf("Cтудент %s %s обучается на факультете %s, номер зачётной книжки %d \n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
+	}
+	char Search[20];
+	while (0==0)
+	{
+		printf("Поиск(чтобы закончить введите Q):"); scanf("%20s",Search);
+		if (strcmp(Search, "Q") == 0)break;
+		for (i = 0; i < 3; i++)
+		{
+			if (strcmp(stud[i].famil, Search) == 0 || strcmp(stud[i].name, Search) == 0 || strcmp(stud[i].facult, Search) == 0 || stud[i].Nomzach == atoi(Search))
+				printf("Cтудент %s %s обучается на факультете %s, номер зачётной книжки %d \n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
+
+		}
+		
+	}
+	_getch();
     return 0;
 }
